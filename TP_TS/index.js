6e0187646665c4c17ad7c88da1299008d8000fc8
guinia <142756394+guinia@users.noreply.github.com>
2023-10-18 23:45:51 -0300

    god

--- TP_TS/index.js
+++ TP_TS/index.js
@@ -1,17 +1,65 @@
+var allProducts = [];
+var sortBy = {};
+var itemsPerPage = 10;
+function filterProducts(value) {
+    var filteredProducts = allProducts.filter(function (p) { return p.title.toLowerCase().includes(value.toLowerCase()) || p.description.toLowerCase().includes(value.toLowerCase()); });
+    drawTable(filteredProducts);
+}
+function paginateProducts(page) {
+    var paginatedProducts = allProducts.slice((page - 1) * itemsPerPage, (page * itemsPerPage) - 1);
+    drawTable(paginatedProducts);
+}
+function sortProducts(prop) {
+    var _a;
+    if (sortBy[prop]) {
+        if (sortBy[prop] === 'asc') {
+            sortBy[prop] = 'desc';
+        }
+        else if (sortBy[prop] === 'desc') {
+            sortBy[prop] = null;
+        }
+    }
+    else {
+        sortBy = (_a = {}, _a[prop] = 'asc', _a);
+    }
+    // @ts-ignore
+    var sortedProducts = allProducts.toSorted(function (a, b) {
+        if (sortBy[prop] === 'asc') {
+            return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+        }
+        else if (sortBy[prop] === 'desc') {
+            return a[prop] < b[prop] ? 1 : a[prop] > b[prop] ? -1 : 0;
+        }
+        else {
+            return allProducts;
+        }
+    });
+    drawTable(sortedProducts);
+}
+function drawTable(product) {
+    // Prepare table HTML
+    var tableHTML = "\n    <thead>\n      <tr>\n        <th><button type=\"button\" class=\"btn btn-link\" onclick=sortProducts(\"id\")>ID</button></th>\n        <th><button type=\"button\" class=\"btn btn-link\" onclick=sortProducts(\"title\")>Title</button></th>\n        <th><button type=\"button\" class=\"btn btn-link\" onclick=sortProducts(\"description\")>Descritpion</button></th>\n        <th><button type=\"button\" class=\"btn btn-link\" onclick=sortProducts(\"price\")>Price</button></th>\n      </tr>\n    </thead>\n    <tbody>\n    ";
+    // Loop thru all characters to generate rows of the table
+    product.forEach(function (p) {
+        tableHTML += "<tr><td>".concat(p.id, "</td><td>").concat(p.title, "</td><td>").concat(p.description, "</td><td>").concat(p.price, "</td></tr>");
+    });
+    // Close table body
+    tableHTML += '</tbody>';
+    // Grab table element to set its inner HTML
+    document.querySelector('#tabla').innerHTML = tableHTML;
+}
 fetch('https://fakestoreapi.com/products')
     .then(function (res) { return res.json(); })
     .then(function (products) {
-
-    var tableHTML = '<thead><tr><th>ID</th><th>Title</th><th>Descritpion</th><th>Price</th></tr></thead><tbody>';
-
-    products.forEach(function (p) {
-        tableHTML += "<tr><td>".concat(p.id, "</td><td>").concat(p.title, "</td><td>").concat(p.description, "</td><td>").concat(p.price, "</td></tr>");
-    });
-
-    tableHTML += '</tbody>';
-
-    document.querySelector('#tabla').innerHTML = tableHTML;
-    
+    allProducts = products;
+    drawTable(products);
+    var pages = Math.ceil(products.length / itemsPerPage);
+    var paginationElement = document.querySelector('#paginationElement');
+    var pagesHTML = '';
+    for (var index = 1; index <= pages; index++) {
+        pagesHTML += "<li class=\"page-item\"><a class=\"page-link\" href=\"#\" onclick=\"paginateProducts(".concat(index, ")\">").concat(index, "</a></li>");
+    }
+    paginationElement.innerHTML = pagesHTML;
     var elementoSpinner = document.querySelector('#containerSpinner');
     elementoSpinner.style.display = 'none';
 });
--- TP_TS/index.ts
+++ TP_TS/index.ts
@@ -13,20 +13,94 @@ type Product = {
     rating: Rating;
 };
 
+let allProducts: Product[] = [];
+let sortBy: { [key: string]: 'asc' | 'desc' | null } = {};
+const itemsPerPage = 10;
+
+function filterProducts(value: String){
+    const filteredProducts: Product[] = allProducts.filter((p: Product) => p.title.toLowerCase().includes(value.toLowerCase()) || p.description.toLowerCase().includes(value.toLowerCase()))
+    drawTable(filteredProducts);
+}
+
+function paginateProducts(page: number){
+    const paginatedProducts: Product[] = allProducts.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+    drawTable(paginatedProducts);
+}
+
+function sortProducts(prop: string) {
+    if (sortBy[prop]) {
+      if (sortBy[prop] === 'asc') {
+        sortBy[prop] = 'desc';
+      } else if (sortBy[prop] === 'desc') {
+        sortBy[prop] = null;
+      }
+    } else {
+      sortBy = { [prop]: 'asc' };
+    }
+  
+    // @ts-ignore
+    const sortedProducts: Product[] = allProducts.toSorted((a: Product, b: Product) => {
+      if (sortBy[prop] === 'asc') {
+        return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+      } else if (sortBy[prop] === 'desc') {
+        return a[prop] < b[prop] ? 1 : a[prop] > b[prop] ? -1 : 0;
+      } else {
+        return allProducts;
+      }
+    });
+  
+    drawTable(sortedProducts);
+}
+
+function drawTable(product: Product[]) {
+
+    // Prepare table HTML
+    let tableHTML: string = `
+    <thead>
+      <tr>
+        <th><button type="button" class="btn btn-link" onclick=sortProducts("id")>ID</button></th>
+        <th><button type="button" class="btn btn-link" onclick=sortProducts("title")>Title</button></th>
+        <th><button type="button" class="btn btn-link" onclick=sortProducts("description")>Descritpion</button></th>
+        <th><button type="button" class="btn btn-link" onclick=sortProducts("price")>Price</button></th>
+      </tr>
+    </thead>
+    <tbody>
+    `;
+
+    // Loop thru all characters to generate rows of the table
+    product.forEach((p: Product) => {
+        tableHTML += `<tr><td>${p.id}</td><td>${p.title}</td><td>${p.description}</td><td>${p.price}</td></tr>`;
+    });
+
+    // Close table body
+    tableHTML += '</tbody>';
+
+    // Grab table element to set its inner HTML
+    document.querySelector('#tabla')!.innerHTML = tableHTML;
+}
+
 fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
     .then((products: Product[]) => {
 
-        let tableHTML: string = '<thead><tr><th>ID</th><th>Title</th><th>Descritpion</th><th>Price</th></tr></thead><tbody>';
+        allProducts = products;
 
-        products.forEach((p: Product) => {
-            tableHTML += `<tr><td>${p.id}</td><td>${p.title}</td><td>${p.description}</td><td>${p.price}</td></tr>`;
-        })
+        drawTable(products);
 
-        tableHTML += '</tbody>'
+        const pages: number = Math.ceil(products.length / itemsPerPage);
 
-        document.querySelector('#tabla')!.innerHTML = tableHTML;
+        const paginationElement: HTMLElement = document.querySelector('#paginationElement')!;
 
+        let pagesHTML: string = '';
+
+        for (let index = 1; index <= pages; index++) {
+            pagesHTML += `<li class="page-item"><a class="page-link" href="#" onclick="paginateProducts(${index})">${index}</a></li>`
+        }
+
+        paginationElement.innerHTML = pagesHTML;
+        
         const elementoSpinner: HTMLElement = document.querySelector('#containerSpinner')!;
         elementoSpinner.style.display = 'none';
+
     });
+
